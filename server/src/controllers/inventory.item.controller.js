3f7ec303ea7df4bcb87ba596b8b0243f8f52a718
Aljas <aljasmuthukadan@gmail.com>
2026-09-02 22:52:44 +0530

    comments on inventory.controllers

--- server/src/controllers/inventory.item.controller.js
+++ server/src/controllers/inventory.item.controller.js
@@ -11,6 +11,15 @@ export const createInventoryItem = async (req, res) => {
   }
 
   try {
+    // Check if an inventory item with the same name already exists
+    const existingItem = await InventoryItem.findOne({ name });
+    if (existingItem) {
+      return res.status(400).json({
+        success: false,
+        message: "An inventory item with the same name already exists",
+      });
+    }
+    // Create a new inventory item
     const newItem = await InventoryItem.create(req.body);
 
     return res.status(201).json({
@@ -29,17 +38,35 @@ export const createInventoryItem = async (req, res) => {
 
 export const getInventoryItems = async (req, res) => {
   // GET /api/inventory
-  const inventoryItems = await InventoryItem.find();
-  return res.status(200).json({
-    success: true,
-    message: "Inventory items retrieved successfully",
-    data: inventoryItems,
-  });
-};
+  //  fetches all inventory items and returns them in the response.
+  try {
+    const inventoryItems = await InventoryItem.find();
+    if (inventoryItems.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No inventory items found",
+      });
+    }
+    // Retrieve all inventory items from the database
+    return res.status(200).json({
+      success: true,
+      message: "Inventory items retrieved successfully",
+      data: inventoryItems,
+    });
+  } catch (error) {
+    console.error("Error fetching inventory items:", error);
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching inventory items",
+    });
+  }
+}
+
 
 export const getInventoryItemByCategory = async (req, res) => {
   // GET /api/inventory/category/:category
   try {
+    // Retrieve inventory items by category from the database
     const { category } = req.params;
     const inventoryItems = await InventoryItem.find({ category });
     if (inventoryItems.length === 0) {
@@ -48,6 +75,7 @@ export const getInventoryItemByCategory = async (req, res) => {
         message: `No inventory items found in category '${category}'`,
       });
     }
+    // Return the inventory items in the response
     return res.status(200).json({
       success: true,
       message: `Inventory items in category '${category}' retrieved successfully`,
